c8b2a06d735d19ed24999382baa1204d0501fcab
Mane <manearakelyan.ait@gmail.com>
2024-08-09 22:01:44 +0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Home from './components/pages/landing-page';
+import Layout from './components/pages/layout';
+
+function App() {
+  return (
+    <div >
+      <Layout>
+        <Home />
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
